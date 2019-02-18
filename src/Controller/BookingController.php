c4eda72ad418123ddfdaf79511e152a2791d476f
travail <travail@macbook-pro-de-frederic-1.home>
2019-02-18 21:26:31 +0100

    gestion de l envoie de mail commencée, mais pas fini

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Dompdf\Dompdf;
 use Stripe\Stripe;
 use App\Entity\Person;
 use App\Entity\Reservation;
@@ -10,6 +11,7 @@ use App\Repository\PersonRepository;
 use App\Repository\ReservationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -77,18 +79,18 @@ class BookingController extends AbstractController
      */
     public function treatment(ObjectManager $manager)
     {
-        /*Stripe::setApiKey("sk_test_AssWuckpnHlwx6B4edglOnpj");
+        Stripe::setApiKey("sk_test_AssWuckpnHlwx6B4edglOnpj");
 
-        $token = $this->get('session')->get('stripeToken');*/
+        $token = $this->get('session')->get('stripeToken');
 
         $price = $this->get('session')->get('price');
 
-        /*$charge = \Stripe\Charge::create([
+        $charge = \Stripe\Charge::create([
             'amount' => $price*100,
             'currency' => 'eur',
             'description' => 'Example charge',
             'source' => $token,
-        ]);*/
+        ]);
 
         $reservationData = $this->get('session')->get('reservation');
         $reservation = $this->prepareReservation($this->get('session')->get('mail'), $manager);
@@ -106,6 +108,8 @@ class BookingController extends AbstractController
         $manager->flush();
 
         // createMailContent
+        // note : je devrais transformer ça en une collection de billets en HTML !!!
+        createMailContent($reservation);
 
         // return view
         return new Response("blabla");
@@ -118,6 +122,58 @@ class BookingController extends AbstractController
      */
     public function createMailContent(Reservation $reservation)
     {
+        $mailSubject = 'Votre réservation pour le musée du Louvre';
+
+        if (preg_match("#(hotmail|live|msn)\.[a-zA-Z]{2,6}$#", $mail))
+            $newLine = "\r\n";
+        else
+            $newLine = "\n";
+
+        $message = array();
+        $message[] = 'Merci pour votre réservation au musée du Louvre.';
+        $message[] = 'Vous avez réservé pour la date du ' . $reservation->stringVisitDay();
+        if ($reservation->getHalfDay())
+            $message[] = 'Votre arrivée est prévue après 14h.';
+        else
+            $message[] = 'Vous pouvez venir à l\'heure que vous désirez.';
+        $message[] = 'Votre numéro de réservation est le' . $reservation->getBookingCode();
+        $message[] = 'coût total de la réservation : ' . $reservation->price() . '€';
+
+        $messageText = '';
+        $messageHtml = '<p>';
+        foreach($message as $paragraph)
+        {
+            $messageText .= $paragraph . $newLine;
+            $messageHtml .= $paragraph . '</p><p>';
+        }
+        $messageHtml = '</p>';
+
+        $finalMail = (new \Swift_Message($mailSubject))
+            ->setSubject($mailSubject)
+            ->setFrom('travail@MacBook-Pro-de-frederic.local')
+            ->setTo($mail)
+            ->setCharset('UTF-8')
+            ->setBody($messageText)
+            ->addPart($messageHtml, 'text/html');
+        
+        $i = 0;
+        foreach ($reservation->getPersons() as $person)
+        {
+            $pdfFile = new Dompdf();
+            $pdfFile->loadHtml($this->templating->render('mail/ticket.html.twig', ['person' => $person, 'reservation' => $reservation]))
+                    ->setPaper('A4', 'portrait')
+                    ->render();
+            
+            $pdfName = $i . '_' . $person.getFirstName() . '_' . $person.getName() . '.pdf';
+            $i++;
+
+            $attachment = new Swift_Attachment($pdfFile, $pdfName, 'application/pdf');
+
+            $finalMail->attach($attachment);
+        }
+
+        $this->get('mailer')->send($mailFinal);
+
 
     }
 
