c192de8d9e5a0735efba47397266b378dda39567
travail <travail@MacBook-Pro-de-frederic.local>
2019-04-12 13:05:39 +0200

    code getting clear, partially removed useless comments, and creates new clear comments

--- public/js/addPerson.js
+++ public/js/addPerson.js
@@ -1,11 +1,26 @@
+// included in templates > booking > index.html.twig
+// adding or deleting persons to the reservation, in the reservation form, using "ajoutez un visiteur" and "supprimer le visiteur" buttons
+
+// we create an index, that we will use to create a unique html id for the person we add to the reservation.
 $index = 0
 
+/*
+ * trigged when clicking on "ajouter un visiteur" button in the reservation form
+ * we add to the collection of persons (that is in the reservation) all the code usefull to create a new person, code we can found in the data prototype of the collection.
+ * then we include the unique index in the html id of the person
+ * then we call the handledeletebuttons function defined below
+*/
 $('#addPerson').click(function(){
     $('#reservation_persons').append($('#reservation_persons').data('prototype').replace(/__name__/g, $index));
     handleDeleteButtons();
     $index++;
 });
 
+/**
+ * search the delete buttons related to the persons
+ * associate to it a function that remove the person
+ * note : data action and target are defined in templates > booking > index.html.twig in block _reservation_persons_entry_row
+ */
 function handleDeleteButtons()
 {
     $('button[data-action="delete"]').click(function(){
@@ -14,4 +29,5 @@ function handleDeleteButtons()
     });
 }
 
+// we call the function before clicking on any buttons to get sure every delete button is handled
 handleDeleteButtons();
--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * controller to manage the case the visitor is in the new booking tunnel
+ */
+
 namespace App\Controller;
 
 use Stripe\Stripe;
@@ -22,22 +26,24 @@ use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 class BookingController extends AbstractController
 {
     /**
+     * when you choose to make a new reservation, this route create a reservation form that you can fill, then it recall this same route for the validation of the form, then redirect to route "payment" when the form is valid.
+     * 
      * @Route("/reservation/nouvelle", name="booking_filling_form")
      */
     public function index(Request $request, ObjectManager $manager)
     {
+        // create the new reservation with the mail used at the homepage
         $mail = $this->get('session')->get('mail');
-
-        // reservation doit être initialiser différement selon que le form ait déjà été rempli ou non ?
-        /* $reservation = $request->request->get('reservation');
-        if ($reservation == null)*/
         $reservation = new Reservation();
         $reservation->setMail($mail);
     
+        // create a reservation form, with the homemade reservationtype, using the reservation object we just created
         $form = $this->createForm(ReservationType::class, $reservation);
         
+        // usefull the second time we get through this route, when it recall itself, after the form has been filled. Handle the request relative to the reservation form.
         $form->handleRequest($request);
         
+        // if the form is submitted and valid, we save the reservation and the price in the session for later. We use the request to get the reservation, because otherwise, the session save just the reservation, without the persons related to it. Then we redirect to the payment route.
         if ($form->isSubmitted() && $form->isValid())
         {
             $this->get('session')->set('reservation', $request->request->get('reservation'));
@@ -46,6 +52,9 @@ class BookingController extends AbstractController
             return $this->redirectToRoute("payment");
         }
 
+        /**
+         * else (if the form isn't submitted yet or if the form isn't valid) we propose to fill the form
+         */
         return $this->render('booking/index.html.twig', [
             'form' => $form->createView(),
             'mail' => $mail
@@ -53,6 +62,9 @@ class BookingController extends AbstractController
     }
 
     /**
+     * first we go through the else, and display the payment page, after passing to it the price multiplied by a hundred to get it in cents. This page implement the stripe solution, that display a form to get the payment informations.
+     * then, once the stripe's form is submitted, we catch the token returned by stripe and go to the treatment route.
+     * 
      * @Route("/paiement", name="payment")
      */
     public function payment()
@@ -72,16 +84,18 @@ class BookingController extends AbstractController
     }
 
     /**
+     * treatment of the reservation
+     * 
      * @Route("/traitement", name="treatment")
      */
     public function treatment(ObjectManager $manager, \Swift_Mailer $mailer, Mail $mailService)
     {
+        // preparing payment
         Stripe::setApiKey("sk_test_AssWuckpnHlwx6B4edglOnpj");
-
         $token = $this->get('session')->get('stripeToken');
-
         $price = $this->get('session')->get('price');
 
+        // processing to payment with stripe
         $charge = \Stripe\Charge::create([
             'amount' => $price*100,
             'currency' => 'eur',
@@ -89,32 +103,33 @@ class BookingController extends AbstractController
             'source' => $token,
         ]);
 
+        // catching boolean indicating weather the payment processed succesfully or not
         $success = ($charge->status == "succeeded");
 
+        // if the payment succeed
         if ($success)
         {
             $mail = $this->get('session')->get('mail');
 
+            // we catch all the reservation datas (including those relative to the persons) we previously (in booking controller > index function > l49) saved in session using the request
             $reservationData = $this->get('session')->get('reservation');
+
+            // we create a new reservation object that we feed with the request object reservationData we just passed through the session
             $reservation = new Reservation();
             $reservation->setMail($mail);
             $form = $this->createForm(ReservationType::class, $reservation);
             $form->submit($reservationData);
 
-            /*foreach($reservation->getTemporaryPersonsList() as $person)
-            {
-                $manager->persist($person);
-                $reservation->addPerson($person);
-            }*/
-
+            // now the payment have been proceed successfully, we can persist the reservation
             $manager->persist($reservation);
 
             $manager->flush();
 
+            // then, we send the tickets for Louvre by mail, with swiftmailer, with a handmade mail service. We don't take care of mails not beeing sent successfully here, because we tell the customer to wait a few minutes, then he can get it later through the consultation path if he never get it otherwise.
             $mailService->sendMail($reservation, $mail, $mailer);
         }
 
-        // return view
+        // then, we display a message telling if the reservation succeed or not.
         return $this->render("booking/treatment.html.twig", [
             'success' => $success,
             'mail' => $mail
